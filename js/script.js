b9cd9065aad715816d390a6b0361225f12758c74
neda1998 <nedagholami1998@gmail.com>
2022-11-12 22:38:50 +0330

    add new codes and styles

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const navMobile = document.getElementById("nav-mobile");
+const iconList = document.getElementById("btn-list")
+
+
+
+
+
+
+// toggle menu
+
+
+function openMenu () {
+    navMobile.classList.toggle("move")
+}
